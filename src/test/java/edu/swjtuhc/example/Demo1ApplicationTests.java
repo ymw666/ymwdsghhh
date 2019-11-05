f85feba3bbc294ced62d0af9793396e149c1d4cd
91631 <91631@LAPTOP-40R1CFHB>
2019-11-05 11:50:20 +0800

    创建项目

---
+++ src/test/java/edu/swjtuhc/example/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.swjtuhc.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
